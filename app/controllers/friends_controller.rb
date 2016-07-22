bdd5b8218f27f2bdf7dbdcad3fdc3816ad13c456
stevula <brodericksteven@gmail.com>
2016-07-22 14:11:12 -0700

    Embed google map

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,7 +2,7 @@ class FriendsController < ApplicationController
   def index
     if current_user
       # p Facebook.get_friends(current_user)
-      p Facebook.get_posts(current_user)
+      # p Facebook.get_posts(current_user)
     end
   end
 end
