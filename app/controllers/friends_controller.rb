6d8b56766444714375c08616829037de011e3c86
stevula <brodericksteven@gmail.com>
2016-07-22 14:59:47 -0700

    Add geo data to user successfully

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,17 @@
 class FriendsController < ApplicationController
   def index
-    @user = current_user
-    if @user
-      location_data = Facebook.get_location(@user)
+    user = current_user
+    if user
+      location_data = Facebook.get_location(user)
       location = location_data['location']['name']
       geodata = Geokit::Geocoders::MultiGeocoder.geocode(location)
       @latitude, @longitude = geodata.lat, geodata.lng
+      user.attributes = {latitude: @latitude, longitude: @longitude}
+      if user.save
+        # TODO
+      else
+        # TODO
+      end
     end
   end
 end
