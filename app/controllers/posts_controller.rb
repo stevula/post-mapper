074f5073517274a17a2e2a3d34e21af797e2ee8c
stevula <brodericksteven@gmail.com>
2016-07-26 00:19:16 -0700

    Test posts helper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,13 @@
 class PostsController < ApplicationController
-  # TODO: handle errors
+  include PostsHelper
+
   def index
     user = current_user
     if user
       @latitude, @longitude = user.update_location
       posts = Facebook.get_posts(user)
       post_data = posts['data']
-      # TODO: move this logic out of controller
-      @posts_with_location = post_data.reduce({data: []}) do |posts, post| 
-        if post['place']
-          location = post['place']['location']
-          coords = [location['latitude'], location['longitude']]
-          author = post['from']['name']
-          message = post['message']
-          post_hash = {coords: coords, author: author, message: message}
-          posts[:data] << post_hash
-        end
-        posts
-      end
-
-      @posts_with_location
+      @posts_with_location = filter_posts_with_location(post_data)
     end
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+  def filter_posts_with_location(post_data)
+    post_data.reduce({data: []}) do |posts, post| 
+      if post['place']
+        location = post['place']['location']
+        coords = [location['latitude'], location['longitude']]
+        author = post['from']['name']
+        message = post['message']
+        post_hash = {coords: coords, author: author, message: message}
+        posts[:data] << post_hash
+      end
+      posts
+    end
+  end
 end
